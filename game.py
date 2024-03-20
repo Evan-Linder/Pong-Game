43df8d8d9d301490e67a9dd5211698afbec5f59c
Evan123 <mo9564ej@go.minnstate.edu>
2024-03-20 17:48:08 -0500

    Progress Created the game class object, added a window and color to the background.

--- game.py
+++ game.py
@@ -1,2 +1,26 @@
+
+import pygame
+
 class Game:
-    pass
+    #create game constants (unmutable).
+    WIDTH, HEIGHT = 700,500
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+
+    def __init__(self):
+        pygame.init()
+        self.win = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        #set background to red.
+        self.win.fill(self.RED)
+        #update display.
+        pygame.display.update()
+
+
+    def run_game(self):
+        # run the game.
+        run = True
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 from game import Game
+
+if __name__=="__main__":
+
+    #create a new isntance of the game class
+    game = Game()
+
+    game.run_game()
